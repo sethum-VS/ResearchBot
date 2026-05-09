dac6b87d8dd547c694e7a057322bd23b3c2b2645
sethum-VS <smwopendev@gmail.com>
2026-05-09 23:54:07 +0530

    feat: migrate to native Xcode project structure and update App source

---
+++ ResearchGraphApp/App/ResearchBot/ResearchBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResearchBotApp.swift
+//  ResearchBot
+//
+//  Created by Sethum Methsanda on 2026-05-09.
+//
+
+import SwiftUI
+
+@main
+struct ResearchBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
